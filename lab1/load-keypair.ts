597794a03414d9f67e4b86c2c6974987d16ea352
cgoretchi <claudiu.goretchi@generiscorp.com>
2024-10-17 17:07:24 +0300

    [SOL] - ignore

--- lab1/load-keypair.ts
+++ lab1/load-keypair.ts
@@ -1,4 +1,6 @@
 import "dotenv/config"
+// import * as dotenv from 'dotenv'
+// dotenv.config({ path: '../.env'})
 import {getKeypairFromEnvironment} from "@solana-developers/helpers";
 
 const loadKeypair = getKeypairFromEnvironment("SECRET_KEY")
